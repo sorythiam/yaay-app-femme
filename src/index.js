d76fa6734d5c36593b928bf43d3bdb62f715e1e4
sorythiam <sorythiam@gmail.com>
2026-05-02 23:06:19 +0000

    Fix index.jsx

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
 // Enregistrement du Service Worker pour PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
